eefc24282728e633d6d64e794d609bd29d28b368
RebeccaEEMartin <rebyk511@msn.com>
2016-06-17 15:45:04 +0100

    HEJUMPSgit add *!

--- game.py
+++ game.py
@@ -6,47 +6,101 @@ pygame.init()
 
 #screen resolution and caption
 display_width = 800
-display_height = 600
+display_height = 400
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('A bit Runny')
 
 #initialise some colours
 black = (0,0,0)
 white = (255,255,255)
-
+red = (255,0,0)
 
 #initialise clock
 clock = pygame.time.Clock()
 
 crashed = False
 
-#initialise car image
-carImg = pygame.image.load('racecar.png')
+guyImg = pygame.image.load('guy.png')
+guy_height = 82
+guy_standing = True
+guy_jumping = False
 
-#display car
-def car(x,y):
-	gameDisplay.blit(carImg, (x,y))
-	
-#starting point for car
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+#display guy
+def guy(x,y):
+    gameDisplay.blit(guyImg, (x,y))
+    
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+    
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+    
+    pygame.display.update()
+    
+    time.sleep(2)
+    
+    game_loop()
+    
+def crash():
+    message_display('You Crashed')
+    
+def game_loop():
+    x = (display_width * 0.25)
+    y = (display_height * 0.7)
 
-########################################################
+    y_change = 0
 
-#GAME LOOP
+    gameExit = False
+    guy_standing = True
+    guy_jumping = False
 
-while not crashed:
+    while not gameExit:
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			crashed = True
-			
-		gameDisplay.fill(white)
-		car(x,y)
-		
-	pygame.display.update()
-	clock.tick(60)
-	
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+
+            if event.type == pygame.KEYDOWN:
+                print "KEYDOWN"
+                if event.key == pygame.K_w and guy_standing == True:
+                    print "JUMPING"
+                    guy_standing = False
+                    guy_jumping = True
+                    y_change = -20
+
+        if guy_jumping == True and y == 180:
+            print "DESCENDING"
+            y_change = 5
+            """if y + y_change + 2 <=280:
+                y_change +=2
+            else:
+                y_change = 280-y"""
+        elif guy_jumping == True and y > 280:
+            print "GROUNDED"
+            y_change = 0
+            y = 280
+            guy_jumping = False
+            guy_standing = True
+        elif guy_standing == True:
+            y_change = 0
+            
+        y += y_change
+        print y
+
+        gameDisplay.fill(white)
+        guy(x,y)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+
+game_loop()
+    
 pygame.quit()
 quit()
